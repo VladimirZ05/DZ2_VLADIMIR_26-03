58e444b2b05d66ed8eda8da82b7947e40e843c45
VladimirZ05 <132680480+VladimirZ05@users.noreply.github.com>
2023-05-10 16:38:02 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,56 +1,37 @@
-var Order = {
-    place: 'StarBucks Menue:',
-    name: 'coffee:',
-    coffeeType: {
-        a: 'Latte',
-        b: 'Americano',
-        c: 'Cappuccino',
-        d: 'Mokiato',
-        e: 'Espresso',
+var StarBucks = {
+    coffee: {
+        latte: 'Latte Coffee',
+        cappuccino: 'Cappuccino Coffe',
+        mokiato: 'Mokiato Coffee',
+        espresso: 'Espresso Coffee'
     },
-    name2: 'tea:',
-    teaType: {
-        a: 'Black Tea',
-        b: 'Green Tea',
-        c: 'White Tea',
-        d: 'Dolong Tea',
-        e: 'Herbal Tea',
+    tea: {
+        black: 'Black Tea',
+        green: 'Green Tea',
+        herbal: 'Herbal Tea',
+        fruit: 'Fruit Tea',
     }
 }
-console.log(Order)
-console.log(Order.place)
-console.log(Order.name)
-console.log(Order.coffeeType.a)
-console.log(Order.coffeeType.b)
-console.log(Order.coffeeType.c)
-console.log(Order.coffeeType.d)
-console.log(Order.coffeeType.e)
-console.log(Order.name2)
-console.log(Order.teaType.a)
-console.log(Order.teaType.b)
-console.log(Order.teaType.c)
-console.log(Order.teaType.d)
-console.log(Order.teaType.e)
-var OrderIn = prompt('Enter your order')
-if (OrderIn === 'Latte') {
-    console.log('Your Coffee order was successfully done! Please, wait for your order ready.')
-} else if (OrderIn === 'Cappuccino') {
-    console.log('Your Coffee order was successfully done! Please, wait for your order ready.')
-} else if (OrderIn === 'Americano') {
-    console.log('Your Coffee order was successfully done! Please, wait for your order ready.')
-} else if (OrderIn === 'Mokiato') {
-    console.log('Your Coffee order was successfully done! Please, wait for your order ready.')
-} else if (OrderIn === 'Espresso') {
-    console.log('Your Coffee order was successfully done! Please, wait for your order ready.')
-} else if (OrderIn === 'Herbal Tea') {
-    console.log('Your Tea order was successfully done! Please, wait for your order ready.')
-} else if (OrderIn === 'Black Tea') {
-    console.log('Your Tea order was successfully done! Please, wait for your order ready.')
-} else if (OrderIn === 'Green Tea') {
-    console.log('Your Tea order was successfully done! Please, wait for your order ready.')
-} else if (OrderIn === 'White Tea') {
-    console.log('Your Tea order was successfully done! Please, wait for your order ready.')
-} else if (OrderIn === 'Dolong Tea') {
-    console.log('Your Tea order was successfully done! Please, wait for your order ready.')
-} else
-    console.log('Your order is not correct or its not avialable now!')
+console.log(StarBucks)
+console.log(StarBucks.coffee)
+console.log(StarBucks.coffee.latte)
+console.log(StarBucks.coffee.cappuccino)
+console.log(StarBucks.coffee.mokiato)
+console.log(StarBucks.coffee.espresso)
+
+console.log(StarBucks.tea)
+console.log(StarBucks.tea.black)
+console.log(StarBucks.tea.green)
+console.log(StarBucks.tea.herbal)
+console.log(StarBucks.tea.fruit)
+
+var Order = prompt('Enter your order')
+if (Order === 'latte' || Order === 'cappuccino' || Order === 'mokiato' || Order === 'espresso'){
+    console.log('Your Coffee order was successfully done, please wait till it ready.')
+}
+else if (Order === 'black' || Order === 'green' || Order === 'herbal' || Order === 'fruit'){
+    console.log('Your Tea order was successfully done, please wait till it ready.')
+}
+else
+    console.log('Your order is not correct or its not avialable now. Please try back later!')
+
